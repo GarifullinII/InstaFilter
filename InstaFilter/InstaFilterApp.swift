03e3883c5a2fbc71c266502c7e261b814b389d21
GarifullinII <ildaridrisovich@gmail.com>
2024-08-07 14:34:18 +0400

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Ildar Garifullin on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
